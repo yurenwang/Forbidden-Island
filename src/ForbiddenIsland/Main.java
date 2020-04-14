a404ab44a6297f67fc24057982aa17f736ac92f0
yurenwang <sywyr2013@gmail.com>
2020-04-13 23:21:36 -0400

    Add main method and heli, pilot images

---
+++ src/ForbiddenIsland/Main.java
@@ -0,0 +1,8 @@
+package ForbiddenIsland;
+
+public class Main {
+    public static void main(String[] args) {
+        ForbiddenIslandWorld world = new ForbiddenIslandWorld();
+        world.bigBang(650, 650, .4);
+    }
+}
